ff78c163772bb27ee03d9bad6a3bb0871a74ddcb
chayanon <chayanon.au@rmuti.ac.th>
2024-01-02 15:44:12 +0700

    add

--- frontend/src/page/Newproduct.js
+++ frontend/src/page/Newproduct.js
@@ -6,7 +6,7 @@ const Newproduct = () => {
       <from>
         <label htmlFor='name'>Name</label>
         <input type={'text'} name="name"></input>
-        
+        <label></label>
       </from>
     </div>
   )
